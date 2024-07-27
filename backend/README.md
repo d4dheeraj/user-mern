2dec56d678bdb4e38932142369c7358d5665b0ba
Dheeraj Jain <dheerajjaina4@gmail.com>
2024-07-27 23:19:09 +0530

    Backend project setup

---
+++ backend/README.md
@@ -0,0 +1,11 @@
+RUN app in dev mode: npm run dev
+RUN app: npm start
+RUN test: npm run test
+
+
+
+
+1. Created the project with npm init
+2. Added gitignore
+3. install express, mongoose, cors, dotenv dependencies
+4. added script commands and type module
---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at port: ${PORT}`);
+});
