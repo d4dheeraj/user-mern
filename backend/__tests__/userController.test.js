505b7d497068cd98d99cf0d8a714e96d4b90a5c3
Dheeraj Jain <dheerajjaina4@gmail.com>
2024-07-28 18:25:00 +0530

    unit test case of create user

--- backend/__tests__/userController.test.js
+++ backend/__tests__/userController.test.js
@@ -1,5 +1,6 @@
-import { getAllUser } from "../controller/user";
+import { getAllUser, createUser } from "../controller/user";
 import User from "../model/User";
+import Joi from "joi";
 
 jest.mock("../model/User");
 
@@ -7,9 +8,15 @@ const apiMockData = [
   {
     firstName: "username",
     lastName: "lastname",
-    email: "username@lastname@domain.com",
+    email: "username.lastname@domain.com",
   },
 ];
+
+const invalidApiMockData = {
+  firstName: "username",
+  lastName: "lastname",
+  email: "username.lastname", //incorrect email
+};
 
 describe("User controller", () => {
   let req;
@@ -23,6 +30,7 @@ describe("User controller", () => {
     };
   });
 
+  //getAllUser
   describe("getAllUser", () => {
     test("should get all the users", async () => {
       User.find.mockResolvedValue(apiMockData);
@@ -58,6 +66,60 @@ describe("User controller", () => {
       expect(res.json).toHaveBeenCalledWith({
         success: false,
         message: "No user found",
+      });
+    });
+  });
+
+  //create user
+  describe("createUser", () => {
+    beforeEach(() => {
+      req.body = apiMockData[0];
+    });
+
+    test("should create a new user", async () => {
+      const mockUser = { save: jest.fn().mockResolvedValue(req.body) };
+      User.mockImplementation(() => mockUser);
+
+      await createUser(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({
+        success: true,
+        message: "User is created successfully",
+        response: req.body,
+      });
+    });
+
+    test("should handle error if database is down", async () => {
+      const errMsg = "Database is not connected";
+      const mockUser = {
+        save: jest.fn().mockRejectedValue(new Error(errMsg)),
+      };
+      User.mockImplementation(() => mockUser);
+
+      await createUser(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({
+        success: false,
+        message: errMsg,
+      });
+    });
+
+    test("should throw error with invalid email", async () => {
+      req.body = invalidApiMockData;
+      const { error } = Joi.object({
+        email: Joi.string().email().required(),
+      }).validate(req.body);
+      const mockUser = { save: jest.fn().mockResolvedValue(req.body) };
+      User.mockImplementation(() => mockUser);
+
+      await createUser(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({
+        success: false,
+        message: error.message,
       });
     });
   });
